632abd0c215c98ae6d7385544e87d5f9df700f84
Paulo Otavio Firmino Silva <spaulo07@grupoamil.com.br>
2018-11-29 16:10:32 -0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/app.scss';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default App;
